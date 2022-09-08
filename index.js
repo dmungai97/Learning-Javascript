62f86c05aa43a1f980abaa56ce5dc34aa3a8ba2e
David Mungai <dmungai97@gmail.com>
2022-09-08 13:11:38 +0300

    MyCommit

---
+++ index.js
@@ -0,0 +1,101 @@
+console.log("hello");
+
+console.log("Hi");
+
+let naming = 21;
+let sum = naming + 2;
+console.log(sum);
+
+function double(number){
+    return number *2
+}
+
+let result = double(3)//store in a variable
+console.log(result)
+
+function calculate(sum){
+    return sum +8;
+}
+
+let calculation = calculate(4);
+console.log(calculation)
+
+function me(him){
+    return him.toUpperCase();
+}
+
+let persona = me("Hello");
+console.log(persona)
+
+function zima(man){
+    return man;
+}
+
+let washa = zima("Kindukulu");
+console.log(washa)
+
+function mambo(gan){
+    return gan;
+}
+
+let gut = mambo(9);
+console.log(gut)
+
+let gido ="SDA";
+console.log(gido);
+
+
+function drogo(reptile){
+    return reptile
+}
+let moth = drogo("Fire");
+
+
+console.log(moth);
+function milk(run){
+    return run;
+}
+let white = milk("Tasty");
+console.log(white)
+
+
+function add(min){
+return min;
+}
+let max = add("Masaa");
+console.log(max);
+
+
+
+/////////////Function
+function nine(am){
+    return am;
+}
+let eru = nine("Jimbo");
+console.log(eru)
+
+
+let tate =" Masculinity";
+function mew(ma){
+    return ma + tate;
+}//no semicolon on 
+let crow =mew("Andrew");
+console.log(crow);
+
+////function  expression stored in variable
+const speak = function(){
+      console.log("Good day");
+};//has semicolon
+speak();
+speak();
+
+////function parameters and arguments
+const talk = function(name ,time){
+    console.log(`good ${time} ${name}`);
+};
+talk("MArio" ,"Morning");
+
+const sleep = function(ben){
+    console.log(`Carson ${ben}`)
+};
+sleep("Mungai");
